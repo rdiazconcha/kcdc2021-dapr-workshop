e5f4d5735dd54191f928ff8040f9f2cb84fee74c
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-04-07 14:41:26 -0500

    .NET 6.0

--- src/DaprHospital.Hospital.Api/Domain/ValueObjects/ProcedureName.cs
+++ src/DaprHospital.Hospital.Api/Domain/ValueObjects/ProcedureName.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 
-namespace DaprHospital.Hospital.Api.Domain.ValueObjects
+namespace DaprHospital.Hospital.Api.Domain.ValueObjects;
+
+public record ProcedureName
 {
-    public record ProcedureName
+    public string Value { get; init; }
+    private ProcedureName(string value)
     {
-        public string Value { get; init; }
-        private ProcedureName(string value)
-        {
-            Value = value;
-        }
+        Value = value;
+    }
 
-        public static ProcedureName Create(string value)
-        {
-            Validate(value);
-            return new ProcedureName(value);
-        }
+    public static ProcedureName Create(string value)
+    {
+        Validate(value);
+        return new ProcedureName(value);
+    }
 
-        private static void Validate(string value)
+    private static void Validate(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
         {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new ArgumentNullException("Invalid procedure name");
-            }
+            throw new ArgumentNullException("Invalid procedure name");
         }
     }
 }
